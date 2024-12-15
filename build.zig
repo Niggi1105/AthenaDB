19f0899b8d20032c6ca29e9bf273251b22d01be1
Niklas Liesch <niklas.liesch17@gmail.com>
2024-12-15 22:32:41 +0100

    adding build client to zig build 	modified:   build.zig 	modified:   src/client/main.zig

--- build.zig
+++ build.zig
@@ -1,27 +1,26 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
 
     const server_exe = b.addExecutable(.{
-        .name = "AthenaDB",
+        .name = "AthenaDBServer",
         .root_source_file = b.path("src/server/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
     const client_exe = b.addExecutable(.{
-        .name = "AthenaDB",
+        .name = "AthenaDBClient",
         .root_source_file = b.path("src/client/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     b.installArtifact(server_exe);
+
     b.installArtifact(client_exe);
 
     const run_server_cmd = b.addRunArtifact(server_exe);

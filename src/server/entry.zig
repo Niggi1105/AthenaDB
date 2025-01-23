621aac63c247eb1ca15f32c9af36d57db10d9943
Niggi1105 <niklas.liesch@protonmail.com>
2025-01-23 16:11:07 +0100

    add entry de- and encoding and tests 	modified:   src/server/entry.zig

--- src/server/entry.zig
+++ src/server/entry.zig
@@ -31,12 +31,14 @@ pub const Entry = struct {
         }
         self.fields.deinit();
     }
+
     pub fn get_field(self: *const Entry, i: usize) !*const Field {
         if (i >= self.fields.items.len) {
             return EntryError.IndexOutOfBounds;
         }
         return &self.fields.items[i];
     }
+
     pub fn get_field_mut(self: *Entry, i: usize) !*Field {
         if (i >= self.fields.items.len) {
             return EntryError.IndexOutOfBounds;
@@ -59,6 +61,10 @@ pub const Entry = struct {
         self.fields.insert(i, f) catch return ServerError.OutOfMemory;
     }
 
+    pub fn append_field(self: *Entry, f: Field) !void {
+        try self.fields.append(f);
+    }
+
     pub fn encode_append_writer(self: *const Entry, w: anytype) !void {
         try w.writeByte('E');
 
@@ -71,8 +77,9 @@ pub const Entry = struct {
         if (bytes[0] != 'E') {
             return EntryError.InvalidBytes;
         }
+        var i: usize = 1;
         for (raw.fields.items) |*field| {
-            field.decode(bytes[1..]);
+            i += try field.decode(bytes[i..]);
         }
     }
 };
@@ -84,4 +91,48 @@ test "update field" {
     try e.add_field(0, Field{ .Int = undefined });
     try e.update_field(0, Field{ .Int = 20 });
     try std.testing.expectEqual(Field{ .Int = 20 }, e.fields.items[0]);
+}
+
+test "entry encode-decode" {
+    const Array = @import("hermes").field.Array;
+    const alloc = std.testing.allocator;
+    var e = Entry.init(alloc);
+    defer e.deinit();
+
+    try e.append_field(Field{ .Int = 10 });
+    try e.append_field(Field{ .Bool = true });
+    try e.append_field(Field{ .Float = std.math.pi });
+    try e.append_field(Field{ .Char = 'H' });
+    try e.append_field(Field{ .OID = 1 });
+
+    var arr = try Array.init(.Int, alloc, 2);
+    try arr.append(Field{ .Int = 1 });
+    try arr.append(Field{ .Int = 2 });
+    try e.append_field(Field{ .Arr = arr });
+
+    var buf = ArrayList(u8).init(alloc);
+    defer buf.deinit();
+    const w = buf.writer();
+    try e.encode_append_writer(w);
+
+    var undef = Entry.init(alloc);
+    defer undef.deinit();
+    try undef.append_field(Field.undef_from_primitive(.Int, alloc));
+    try undef.append_field(Field.undef_from_primitive(.Bool, alloc));
+    try undef.append_field(Field.undef_from_primitive(.Float, alloc));
+    try undef.append_field(Field.undef_from_primitive(.Char, alloc));
+    try undef.append_field(Field.undef_from_primitive(.OID, alloc));
+    try undef.append_field(Field.undef_from_primitive(.Arr, alloc));
+
+    const bytes = try buf.toOwnedSlice();
+    defer alloc.free(bytes);
+    try undef.decode(bytes);
+
+    try std.testing.expectEqualSlices(Field, e.fields.items[0..5], undef.fields.items[0..5]);
+
+    const s = try e.fields.items[5].Arr.arr.toOwnedSlice();
+    defer alloc.free(s);
+    const u = try undef.fields.items[5].Arr.arr.toOwnedSlice();
+    defer alloc.free(u);
+    try std.testing.expectEqualSlices(Field, s, u);
 }
